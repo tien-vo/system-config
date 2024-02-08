6b2ea0f54fa2a78e904b860c0e91c13f536615cc
tvo <tvo.work@proton.me>
2024-02-07 21:33:48 -0700

    Mako: Update theming

--- modules/user/daemon/mako/default.nix
+++ modules/user/daemon/mako/default.nix
@@ -1,5 +1,14 @@
 { settings, ... }:
 let
+  inherit (builtins) ceil toString;
+  inherit (settings.theme.colorScheme) palette;
+  inherit (settings.theme) font;
+  inherit (settings.window) border;
+
+  PT_TO_PX = 1.334;
+  borderRadius = ceil(PT_TO_PX * border.radius);
+  borderSize = ceil(PT_TO_PX * border.size);
+  fontSize = ceil(PT_TO_PX * font.size);
 in
 {
   config.services.mako = {
@@ -8,5 +17,9 @@ in
     width = 400;
     margin = "20,20";
     defaultTimeout = 5000;
+    backgroundColor = "#${palette.base00}";
+    borderColor = border.active_color.top;
+    font = "${font.name} ${toString(fontSize)}";
+    inherit borderRadius borderSize;
   };
 }
