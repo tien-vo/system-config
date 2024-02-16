c044e416706332eb1c51b40f285b0bb234736012
tvo <tvo.email@proton.me>
2024-02-15 23:20:34 -0700

    Git: Change public email

--- modules/user/devenv/git/default.nix
+++ modules/user/devenv/git/default.nix
@@ -3,7 +3,7 @@
   config.programs.git = {
     enable = true;
     userName = "tvo";
-    userEmail = "tvo.work@proton.me";
+    userEmail = "tvo.email@proton.me";
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
