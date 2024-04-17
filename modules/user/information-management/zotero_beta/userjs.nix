4a30c73592f2ec73c59da46708af1b6d0140c073
tvo <tvo.email@proton.me>
2024-04-16 23:11:30 -0600

    Zotero: Migrate v6 options

--- modules/user/information-management/zotero_beta/userjs.nix
+++ modules/user/information-management/zotero_beta/userjs.nix
@@ -6,6 +6,7 @@ pkgs.writeTextFile ({
   name = "user.js";
   text = ''
     user_pref("extensions.zotero.dataDir", "${dataHome}/zotero");
+    user_pref("extensions.zotero.export.quickCopy.setting", "export=ca65189f-8815-4afe-8c8b-8c7c15f0edca");
     user_pref("extensions.zotero.recursiveCollections", false);
     user_pref("extensions.zotero.automaticSnapshots", false);
     user_pref("extensions.zotero.automaticTags", false);
@@ -13,5 +14,13 @@ pkgs.writeTextFile ({
     user_pref("extensions.zotero.sortCreatorAsString", true);
     user_pref("extensions.zotero.secondarySort.title", "year");
     user_pref("extensions.zotero.secondarySort.firstCreator", "year");
+
+    user_pref("extensions.zotero.translators.better-bibtex.citekeyFold", false);
+    user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + (authini.len(\">\", 1) ? \"+\" : \"\") + year + infix(start=1)");
+    user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + (authini.len(\">\", 1) ? \"+\" : \"\") + year + infix(start=1)");
+
+    user_pref("extensions.zotmoov.dst_dir", "~/workspace/library/assets/zotero");
+    user_pref("extensions.zotmoov.enable_subdir_move", true);
+    user_pref("extensions.zotmoov.subdirectory_string", "{%a}");
   '';
 })
