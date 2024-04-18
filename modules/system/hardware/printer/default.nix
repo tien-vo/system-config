8e8fd032c5882bc09e9a0c3abe13e6263c8e6d01
tvo <tvo.email@proton.me>
2024-04-17 22:29:23 -0600

    Vpn: Add cisco openconnect

--- modules/system/hardware/printer/default.nix
+++ modules/system/hardware/printer/default.nix
@@ -1,6 +1,13 @@
 { ... }:
 {
-  config.services.printing.enable = true;
+  config.services.printing = {
+    enable = true;
+    allowFrom = [
+      "from localhost"
+      "from 127.0.0.1"
+    ];
+    logLevel = "debug";
+  };
   config.services.avahi = {
     enable = true;
     nssmdns4 = true;
