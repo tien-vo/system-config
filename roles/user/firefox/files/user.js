6851f4f3d05f9d8d43fa69b708a8d4d345e3edb4
Tien Vo <tvo.work@proton.me>
2024-05-13 15:16:47 -0600

    Librewolf: Change to using ESR with librewolf's policies

---
+++ roles/user/firefox/files/user.js
@@ -0,0 +1,13 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("extensions.activeThemeID", "{f9be3857-6894-4d6e-90e0-64451bdf7655}");
+
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.startup.homepage", "https://startpage.com");
+user_pref("browser.startup.page", 3);
+user_pref("browser.search.separatePrivateDefault", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
+user_pref("signon.rememberSignons", true);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
