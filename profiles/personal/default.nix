557b2ba5d067f59015b7da8461cd1511b1423d16
Tien Vo <tvo.work@proton.me>
2024-09-25 10:01:30 -0600

    Profiles: Add a personal profile

---
+++ profiles/personal/default.nix
@@ -0,0 +1,65 @@
+{ pkgs, settings, ... }:
+let
+  inherit (settings) src;
+in
+{
+  imports = [
+    "${src}/modules/user/tools/nix"
+    "${src}/modules/user/tools/xdg"
+    #  "${src}/modules/user/tools/ssh"
+    #  "${src}/modules/user/tools/vpn"
+    #  "${src}/modules/user/tools/fcitx5"
+
+    #  "${src}/modules/user/hardware/network"
+    #  "${src}/modules/user/hardware/bluetooth"
+
+    #  "${src}/modules/user/daemon/syncthing"
+    #  "${src}/modules/user/daemon/udiskie"
+
+    #  "${src}/modules/user/devenv"
+    #  "${src}/modules/user/devenv/git"
+    #  "${src}/modules/user/devenv/micromamba"
+
+    #  "${src}/modules/user/console/shell/zsh"
+    #  "${src}/modules/user/console/neovim"
+    #  "${src}/modules/user/console/autojump"
+    #  "${src}/modules/user/console/alacritty"
+
+    #  "${src}/modules/user/theme"
+    #  "${src}/modules/user/window-manager/wayland/hyprland"
+
+    #  "${src}/modules/user/information-management/logseq"
+    #  "${src}/modules/user/information-management/thunderbird"
+    #  "${src}/modules/user/information-management/zotero_beta"
+    #  "${src}/modules/user/information-management/obsidian"
+
+    #  "${src}/modules/user/app/multi-media"
+    #  "${src}/modules/user/app/web-browser/firefox"
+    #  "${src}/modules/user/app/file-manager/vifm"
+    #  "${src}/modules/user/app/file-manager/thunar"
+    #  "${src}/modules/user/app/file-manager/zathura"
+    #  "${src}/modules/user/app/cloud-service/megasync"
+    #  "${src}/modules/user/app/cloud-service/cryptomator"
+    #  "${src}/modules/user/app/office/libreoffice"
+  ];
+
+  config.home.packages = [
+    #  pkgs.xdg-utils
+    #  pkgs.unzip
+    #  pkgs.wl-clipboard
+    #  pkgs.swayimg
+    #  pkgs.htop
+    #  pkgs.neofetch
+    #  pkgs.dex
+    #  pkgs.zoom-us
+    #  pkgs.file
+    #  pkgs.imagemagick
+    #  pkgs.okular
+    #  pkgs.imv
+    #  pkgs.tomato-c
+    #  pkgs.vlc
+    #  pkgs.sshfs
+    #  pkgs.nh
+    #  pkgs.ventoy-full
+  ];
+}
