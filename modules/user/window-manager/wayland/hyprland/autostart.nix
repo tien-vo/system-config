f7c8f5efd50e4cd3cec0bb9ee0d2cabb608c2061
tvo <tvo.email@proton.me>
2024-10-14 13:20:14 -0600

    Hyprland: Starting mako with systemd

--- modules/user/window-manager/wayland/hyprland/autostart.nix
+++ modules/user/window-manager/wayland/hyprland/autostart.nix
@@ -12,10 +12,6 @@ let
       run "${pkgs.libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1"
       run ${pkgs.hyprpaper}/bin/hyprpaper
       run ${pkgs.waybar}/bin/waybar
-      run ${pkgs.mako}/bin/mako
-      run ${pkgs.networkmanagerapplet}/bin/nm-applet
-      run ${pkgs.blueman}/bin/blueman-applet
-      #  run ${pkgs.udiskie}/bin/udiskie --smart-tray
 
       # Kludge for pyprland cause it started generating this wrapped binary
       [[ -z $(pgrep -f ${pkgs.pyprland}/bin/.pypr-wrapped) ]] && \
