ac5b054130660ac9a14c9758aea0c994ba996a68
tien-vo <tvo.email@proton.me>
2025-04-16 11:30:39 -0600

    Git: Change username

--- modules/user/devenv/git/default.nix
+++ modules/user/devenv/git/default.nix
@@ -1,12 +1,12 @@
 { ... }:
 {
-  config.programs.git = {
-    enable = true;
-    userName = "tvo";
-    userEmail = "tvo.email@proton.me";
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = true;
+    config.programs.git = {
+        enable = true;
+        userName = "tien-vo";
+        userEmail = "tvo.email@proton.me";
+        extraConfig = {
+            init.defaultBranch = "main";
+            pull.rebase = true;
+        };
     };
-  };
 }
