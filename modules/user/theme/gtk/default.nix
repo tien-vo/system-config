14b43130c49b8354b2f6496a412164c245de9cd2
tien-vo <tvo.email@proton.me>
2025-06-27 14:43:18 -0500

    Gtk: GTK is still broken but at least it's not complaining anymore

--- modules/user/theme/gtk/default.nix
+++ modules/user/theme/gtk/default.nix
@@ -1,10 +1,15 @@
 { config, pkgs, settings, ... }:
-{
+let
+  inherit (settings.theme) font cursorTheme;
+in {
+  config.home.packages = [
+    pkgs.dconf
+  ];
   config.gtk = {
     enable = true;
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
-    inherit (settings.theme) theme font iconTheme cursorTheme;
+    #  inherit (settings.theme) font cursorTheme;
   };
 }
