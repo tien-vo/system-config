c2a3ff67348b61bd36a7950b986807be234a4a97
tien-vo <tvo.email@proton.me>
2025-08-01 12:21:49 -0600

    Neovim: Update barbar config, bbye is not needed anymore

--- modules/user/console/neovim/config/lua/plugins/ux/closing_buffer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "moll/vim-bbye",
-    lazy = false,
-    keys = {
-        { "<S-h>", ":BufferPrevious<CR>", desc = "Move to previous buffer" },
-        { "<S-l>", ":BufferNext<CR>", desc = "Move to next buffer" },
-        { "<C-c>", ":Bwipeout<CR>", desc = "Close buffer" },
-    }
-}
