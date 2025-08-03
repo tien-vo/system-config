bb733d3560f29c3c7182de08917f3a88d82911db
tien-vo <tvo.email@proton.me>
2025-08-03 13:50:03 -0600

    Neovim: Fix the keysetting issue

--- modules/user/console/neovim/config/lua/plugins/ui/tabs.lua
+++ modules/user/console/neovim/config/lua/plugins/ui/tabs.lua
@@ -4,34 +4,37 @@ return {
         "lewis6991/gitsigns.nvim",
         "nvim-tree/nvim-web-devicons",
     },
-    keys = {
-        { "<S-h>", ":BufferPrevious<CR>", desc = "Move to previous buffer" },
-        { "<S-l>", ":BufferNext<CR>", desc = "Move to next buffer" },
-        { "<C-c>", ":BufferClose<CR>", desc = "Close buffer" },
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-        animation = true,
-        tabpages = true,
-        clickable = false,
-        auto_hide = false,
-        icon_custom_colors = false,
-        insert_at_end = true,
-        insert_at_start = false,
-        hide = {current = false, inactive = false, visible = false},
-        semantic_letters = true,
-        letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-        no_name_title = nil,
-        icons = {
-            filetype = { enabled = true },
-            active = { separator = { left = "▎" } },
-            inactive = { separator = { left = "▎" } },
-            pinned = { button = "車" },
-            modified = { button = "●" },
-            button = "",
-        },
-        sidebar_filetypes = {
-            NvimTree = true
-        }
-    }
+    config = function()
+        local map = vim.api.nvim_set_keymap
+        local opts = { noremap = true, silent = true }
+
+        map("n", "<S-h>", "<Cmd>BufferPrevious<CR>", opts)
+        map("n", "<S-l>", "<Cmd>BufferNext<CR>", opts)
+        map("n", "<C-c>", "<Cmd>BufferClose<CR>", opts)
+
+        require("barbar").setup({
+            animation = true,
+            auto_hide = false,
+            tabpages = true,
+            clickable = false,
+            hide = {current = false, inactive = false, visible = false},
+            icon_custom_colors = false,
+            insert_at_end = true,
+            insert_at_start = false,
+            semantic_letters = true,
+            sidebar_filetypes = {
+                NvimTree = { text = "File Tree", align = "center" },
+            },
+            letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
+            no_name_title = nil,
+            icons = {
+                filetype = { enabled = true },
+                active = { separator = { left = "▎" } },
+                inactive = { separator = { left = "▎" } },
+                pinned = { button = "車" },
+                modified = { button = "●" },
+                button = "",
+            },
+        })
+    end,
 }
