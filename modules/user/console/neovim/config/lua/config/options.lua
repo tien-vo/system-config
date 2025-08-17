7e99edd4b1301a164a9f15ae64b915bdfab06aaa
tien-vo <tvo.email@proton.me>
2025-08-17 16:13:42 -0600

    Neovim: Update lazy config per the documentaion

--- modules/user/console/neovim/config/lua/config/options.lua
+++ modules/user/console/neovim/config/lua/config/options.lua
@@ -1,3 +1,8 @@
+-- These are essential for lazy.nvim
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 -- Indent behaviors
 vim.opt.expandtab = true
 vim.opt.smartindent = true
