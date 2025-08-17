fedff77f54fb2f0a2ac9478ab634042069832235
tien-vo <tvo.email@proton.me>
2025-08-17 15:32:34 -0600

    Neovim: Remove timeout for lsp

--- modules/user/console/neovim/config/lua/plugins/engines/lsp.lua
+++ modules/user/console/neovim/config/lua/plugins/engines/lsp.lua
@@ -42,7 +42,6 @@ return {
     dependencies = {
         { "mason-org/mason.nvim", version = "^1.0.0" },
         { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-        "hinell/lsp-timeout.nvim",
     },
     init = function(_)
         require("mason").setup()
