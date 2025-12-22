e480574d189cf72accd8e77ed754eae6131cffac
tien-vo <work@tienvo.me>
2025-12-22 03:07:11 -0700

    Zsh: Update application names

--- modules/user/console/shell/zsh/environment.nix
+++ modules/user/console/shell/zsh/environment.nix
@@ -3,26 +3,34 @@
   config.programs.zsh.envExtra = ''
     export LANG="en_US.UTF-8"
     export LC_TIME="en_US.UTF-8"
+    export TERM="xterm-256color"
 
     export CARGO_HOME="${config.xdg.dataHome}/cargo"
     export GNUPGHOME="${config.xdg.dataHome}/gnupg"
     export NUGET_PACKAGES="${config.xdg.dataHome}/nuget"
+    export LEIN_HOME="${config.xdg.dataHome}/lein"
+    export PATH="$PATH:$HOME/.local/bin"
+
+    # Python
     export PYTHONSTARTUP="${config.xdg.configHome}/python/pythonrc"
     export PYTHON_HISTORY="${config.xdg.stateHome}/python/history"  # Applied in python 3.13
     export PYTHONPYCACHEPREFIX="${config.xdg.cacheHome}/python"
     export PYTHONUSERBASE="${config.xdg.dataHome}/python"
-    export MATHEMATICA_USERBASE="${config.xdg.configHome}/mathematica"
     export JUPYTER_CONFIG_DIR="${config.xdg.configHome}/jupyter"
     export JUPYTER_PLATFORM_DIRS="1"
-    export LEIN_HOME="${config.xdg.dataHome}/lein"
-    export PATH="$PATH:$HOME/.local/bin"
+
+    # Mathematica
+    export MATHEMATICA_USERBASE="${config.xdg.configHome}/mathematica"
 
     # IDL
-    . "${config.home.homeDirectory}/Apps/idl/9.1.0/idl/bin/idl_setup.bash"
+    . "${config.home.homeDirectory}/01_applications/idl/9.1.0/idl/bin/idl_setup.bash"
     export SPEDAS_DATA_DIR="${config.xdg.dataHome}/spedas"
     export ROOT_DATA_DIR="${config.xdg.dataHome}/spedas"
-    export IDL_PATH="<IDL_DEFAULT>:+${config.home.homeDirectory}/Apps/spedas/bleeding_edge"
+    export IDL_PATH="<IDL_DEFAULT>:+${config.home.homeDirectory}/01_applications/spedas/bleeding_edge"
     export IDL_PATH="$IDL_PATH:+${config.home.homeDirectory}/Workspace/projects/_tvolib/tvolib/src/idl"
-    export TERM="xterm-256color"
+
+    # CDF
+    export PATH="$PATH:$HOME/01_applications/cdf/3.9/bin"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/01_applications/cdf/3.9/lib"
   '';
 }
