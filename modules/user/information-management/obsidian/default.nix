9055852755c59904b80f9ab9643b50e8bb551d52
tien-vo <work@tienvo.me>
2025-12-23 00:41:05 -0700

    Obsidian: default setting is nonsense rn if not setting a vault

--- modules/user/information-management/obsidian/default.nix
+++ modules/user/information-management/obsidian/default.nix
@@ -2,45 +2,44 @@
 {
   config.programs.obsidian = {
     enable = true;
-    defaultSettings = {
-      app = {
-        promptDelete = false;
-        vimMode = true;
-        alwaysUpdateLinks = true;
-        newFileLocation = "folder";
-        newFileFolderPath = "notes";
-      };
-      appearance = {
-        cssTheme = "Material Gruvbox";
-      };
-      themes = [
-        "Material Gruvbox"
-      ];
-      communityPlugins = [
-        "obsidian-excalidraw-plugin"
-        "pdf-plus"
-      ];
-      corePlugins = [
-        "file-explorer"
-        "global-search"
-        "switcher"
-        "graph"
-        "backlink"
-        "canvas"
-        "outgoing-link"
-        "tag-pane"
-        "page-preview"
-        "daily-notes"
-        "templates"
-        "note-composer"
-        "command-palette"
-        "editor-status"
-        "bookmarks"
-        "outline"
-        "word-count"
-        "file-recovery"
-        "bases"
-      ];
-    };
+    #  defaultSettings = {
+    #    app = {
+    #      promptDelete = false;
+    #      vimMode = true;
+    #      alwaysUpdateLinks = true;
+    #      newFileLocation = "folder";
+    #      newFileFolderPath = "notes";
+    #    };
+    #    appearance = {
+    #      cssTheme = "Material Gruvbox";
+    #    };
+    #    themes = [
+    #      "Material Gruvbox"
+    #    ];
+    #    communityPlugins = [
+    #      "obsidian-excalidraw-plugin"
+    #      "pdf-plus"
+    #    ];
+    #    corePlugins = [
+    #      "file-explorer"
+    #      "global-search"
+    #      "switcher"
+    #      "graph"
+    #      "backlink"
+    #      "canvas"
+    #      "outgoing-link"
+    #      "tag-pane"
+    #      "page-preview"
+    #      "daily-notes"
+    #      "templates"
+    #      "note-composer"
+    #      "command-palette"
+    #      "editor-status"
+    #      "bookmarks"
+    #      "outline"
+    #      "word-count"
+    #      "file-recovery"
+    #    ];
+    #  };
   };
 }
