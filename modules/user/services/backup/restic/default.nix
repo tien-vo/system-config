7d6cc93f8f479b4a342b8784176978be76ae2d9f
tien-vo <work@tienvo.me>
2026-07-24 20:50:56 -0700

    Restic: Similar proton workflow failing, exploring alternatives

--- modules/user/services/backup/restic/default.nix
+++ modules/user/services/backup/restic/default.nix
@@ -41,32 +41,30 @@ in
   ];
 
   config.sops = {
-    defaultSopsFile =
-      "${settings.src}/secrets/restic-filen-fw13.yaml";
-
     defaultSopsFormat = "yaml";
-
-    age.keyFile =
-      "${config.xdg.configHome}/sops/age/keys.txt";
-
+  
+    age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+  
     secrets = {
       restic-filen-password = {
+        sopsFile = "${settings.src}/secrets/restic-filen-fw13.yaml";
+        key = "restic-filen-password";
         mode = "0400";
       };
-
+      restic-proton-password = {
+        sopsFile = "${settings.src}/secrets/restic-proton-fw13.yaml";
+        key = "restic-proton-password";
+        mode = "0400";
+      };
       rclone-config-password = {
-        sopsFile =
-          "${settings.src}/secrets/rclone-fw13.yaml";
-
+        sopsFile = "${settings.src}/secrets/rclone-fw13.yaml";
+        key = "rclone-config-password";
         mode = "0400";
       };
     };
-
-    templates."rclone-filen-fw13.env" = {
-      content = ''
-        RCLONE_CONFIG_PASS=${config.sops.placeholder.rclone-config-password}
-      '';
-
+  
+    templates."rclone-fw13.env" = {
+      content = ''RCLONE_CONFIG_PASS=${config.sops.placeholder.rclone-config-password}'';
       mode = "0400";
     };
   };
